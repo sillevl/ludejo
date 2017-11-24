416e94bbccf34b55f1bb36710c1baddf5fe67751
Jasper Vercnocke <jasper.vercnocke@vives.student.be>
2017-11-24 17:54:18 +0100

    Detail map for monsters

---
+++ app/assets/javascripts/monster_detail.js
@@ -0,0 +1,11 @@
+    $(document).on('turbolinks:load', function() {
+
+    window.mymap = L.map('detail_map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoicmVwc2FqamoiLCJhIjoiY2phZTRyaHloMXNieDMzcjF2czdxZmFwMSJ9.Xd4rx5sAMSeK60eO_Qq2yA'
+    }).addTo(window.mymap);
+});
