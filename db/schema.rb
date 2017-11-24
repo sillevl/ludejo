e192b3331041279b0ba77a2e059328b1906afb43
Sille Van Landschoot <info@sillevl.be>
2017-11-24 12:22:26 +0100

    add seed script for populating database with monsters

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124101417) do
+ActiveRecord::Schema.define(version: 20171124111649) do
 
   create_table "feeds", force: :cascade do |t|
     t.decimal "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "monster_id"
   end
 
   create_table "monsters", force: :cascade do |t|
