2d0d9d44c87651600bc38f494d264e5fa487e514
Sille Van Landschoot <info@sillevl.be>
2017-11-24 20:48:14 +0100

    add colorized progressbar

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,3 +1,9 @@
 class Monster < ApplicationRecord
     has_many :feeds
+
+    def health_color
+        return "" if health.to_i > 50
+        return "orange" if (25..50).include?(health.to_i)
+        return "red" if health.to_i < 25
+    end
 end
